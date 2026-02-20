0e7e8cc2894c7ecd876ec4545145bf15106f9462
Aleksey Turchenkov <alexroodo6@gmail.com>
2026-02-20 14:20:51 +0500

    new chirp validation endpoint

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -19,6 +20,7 @@ func main() {
 
 	mux := http.NewServeMux()
 	mux.Handle("/app/", http.StripPrefix("/app", cfg.middlewareMetricsInc(http.FileServer(http.Dir("./")))))
+	mux.HandleFunc("POST /api/validate_chirp", handlerValidateChirp)
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
 	mux.HandleFunc("GET /admin/metrics", cfg.handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", cfg.handlerReset)
@@ -32,23 +34,57 @@ func main() {
 	log.Fatal(srv.ListenAndServe())
 }
 
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	w.Header().Add("content-type", "application/json")
+	w.WriteHeader(code)
+	w.Write([]byte(fmt.Sprintf(`{"error":"%s"}`, msg)))
+}
+
+func respondWithJson(w http.ResponseWriter, code int, payload interface{}) {
+	w.Header().Add("content-type", "application/json")
+	w.WriteHeader(code)
+	w.Write([]byte(fmt.Sprintf("%s", payload)))
+}
+
+func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		log.Printf("Error decoding parameters: %s", err)
+		respondWithError(w, http.StatusInternalServerError, "Something went wrong")
+		return
+	}
+
+	if len(params.Body) > 140 {
+		respondWithError(w, http.StatusBadRequest, "Chirp is too long")
+		return
+	}
+
+	response := `{"valid":true}`
+	respondWithJson(w, http.StatusOK, response)
+}
+
 func handlerReadiness(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("content-type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(http.StatusText(http.StatusOK)))
+	respondWithJson(w, http.StatusOK, http.StatusText(http.StatusOK))
 }
 
 func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("content-type", "text/html; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf(
-		`
+	response := fmt.Sprintf(`
 <html>
   <body>
     <h1>Welcome, Chirpy Admin</h1>
     <p>Chirpy has been visited %d times!</p>
   </body>
-</html>`, cfg.fileserverHits.Load())))
+</html>`, cfg.fileserverHits.Load())
+
+	w.Header().Add("content-type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(response))
 }
 
 func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
